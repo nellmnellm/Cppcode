6d24caccb3c4b5717ffcfe1de092e958995fded7
nellmnellm <132122596+nellmnellm@users.noreply.github.com>
2023-10-02 23:31:27 +0900

    23.10.02(1)

---
+++ x64/guitar/bj2475.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		int num;
+		cin >> num;
+		sum += num * num;
+	}
+
+	cout << sum % 10;
+
+
+	return 0;
+}
