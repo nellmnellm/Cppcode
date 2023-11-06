2c3dcaf9303d8ed54fa6ceebaff0bc574232fb3c
nellmnellm <132122596+nellmnellm@users.noreply.github.com>
2023-11-06 20:08:18 +0900

    23.11.06(1)

--- x64/14. 집합과 맵/bj7785-3.cpp
+++ x64/14. 집합과 맵/bj7785-3.cpp
@@ -1,50 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <map>
 #include <string>
 #include <algorithm>
 using namespace std;
 
 
-bool daesomusi(const string& a, const string& b)
-{
-	string a_lower = a;
-	string b_lower = b;
-	transform(a_lower.begin(), a_lower.end(), a_lower.begin(), ::tolower);
-	transform(b_lower.begin(), b_lower.end(), b_lower.begin(), ::tolower);
-	return a_lower <= b_lower;
-
-}
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	
-
-
 	int N;
 	cin >> N;
-	map <string, string> work;
-	vector <string> vec;
+
+	map <string, bool> map;
+
 	for (int i = 0; i < N; i++)
 	{
-		string s; string t;
-		cin >> s >> t;
-		work[s] = t;	
-		vec.push_back(s);
+		string name; string churip;
+		cin >> name >> churip;
+		if (churip == "enter")
+			map[name] = 1;
+		else
+			map[name] = 0;
 	}
 
-	vector <string> nowwork;
-	for (int i = 0; i < vec.size(); i++)
+	for (auto it = map.rbegin(); it != map.rend(); it++)
 	{
-		if (work[vec[i]] == "enter")
-			nowwork.push_back(vec[i]);
+		if (map[it->first])
+			cout << it->first << '\n';
 	}
 
-	sort(nowwork.begin(), nowwork.end(), daesomusi);
 
-	for (int i = nowwork.size()-1; i >=0; i--)
-		cout << nowwork[i] << "\n";
+	
 	return 0;
 }
